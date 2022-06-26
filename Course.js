b4f8160ceb877824de3b542eb8e19fb863a74785
Nikhil Singh <87113437+Nick6398@users.noreply.github.com>
2022-06-26 22:34:50 +0530

    Update Course.js

--- Course.js
+++ Course.js
@@ -1,102 +1,111 @@
-import { StyleSheet, Text, View ,Image} from 'react-native';
-import React from "react";
-import Menu from '../component/Menu';
+import { FlatList, StyleSheet, Text, View ,Image,TouchableOpacity} from 'react-native';
+import React from 'react';
+import Courses from '../api/Courseapi';
+const Course = ({navigation}) => {
 
-const Home = (propsr) => {
-  const description = "vision :\n\nTo impart quality education for producing highly talented globally recognizable technocrats and entrepreneurs with sound ethics, latest knowledge and innovative ideas in Computer Science and Engineering to meet industrial needs and societal expectations.";
+    const courseCard =({item}) => {
+      return(
+         <View style={styles.mainContainer}>
+        <View style={styles.courseContainer}>
 
-  return (
-    <View style={styles.mainContainer}>
-      <View style={styles.homeTop}>
-       <Image 
-        style={styles.headerImage}
-        resizeMode="contain"
-        source={require("../../assets/nick.jpeg")}
-         />
+          <View>
+            <Image style={styles.cardImage}
+            source={item.image}
+            resizeMode ="contain"
+            />
+          </View> 
 
-         <Text style={styles.mainHeader}>WELCOME TO GRAPHIC ERA </Text>
-         <Text 
-            style={[
-              styles.mainHeader,
-           {   
-              fontSize: 20,
-              color : "#4c5dab",
-              marginTop: 0,
-              
-           },
-          ]}>
-          {propsr.channelName}
-          </Text>
-          <Text style={styles.paraStyle}> {description} </Text>
-     </View>
-     <View style={styles.menuStyle}>
-     <View style={styles.lineStyle}></View>
-      <Menu />
-      <View 
-        style={[
-          styles.lineStyle,
-          {
-            marginVertical:10,
-        
-          },
-          ]}></View>
-     </View>
-    </View>
+          <Text style={styles.mainHeader}>{item.title}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.buttonStyle}
+              onPress={
+              () => navigation.navigate("CourseDetails",
+              {
+                courseId:item.id,
+              })
+              }>
+              <Text style={styles.buttonText}>semester Details</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+
+      </View>
+      );
+    };
+  return(
+     <FlatList
+       keyExtractor={(item) => item.id}
+      data={Courses}
+      renderItem={courseCard}
+  />
   );
 };
 
-
 const styles = StyleSheet.create({
-  mainContainer: {
-    height:"90%",
+  cardImage: {
+    width: "100%",
+    height: undefined,
+    aspectRatio: 1,
+    paddingBottom:20,
+    
+  },
+  mainContainer:{
+  paddingHorizontal:20,
+  },
+  courseContainer:{
+    padding:30,
+    backgroundColor:"rgba(255,255,255,0.90",
+    textAlign:"center",
+    borderRadius:5,
+    shadowColor:"grey",
+    shadowOffset:{width:0, height:0},
+    shadowOpacity:0.5,
+    shadowRadius:8,
+    elevation:8,
+    marginVertical:30,
+  },
+  mainHeader:{
+    fontSize:29,
+    color:"#344055",
+    textTransform:"uppercase",
+    //fontweight:500,
+    paddingBottom:15,
+    textAlign:"center",
+    // fontFamily:"Nunito_600SemiBold",
+  },
+  description:{
+    textAlign:"center",
+    // fontFamily:"JosefinSans_500Medium",
+    paddingBottom:15,
+    lineHeight:20,
+    fontSize:16,
+    color:"navy",
+  },
+ 
+  buttonContainer: {
     display:"flex",
-    justifyContent: "space-between",
-    paddingHorizontal: 19,
-    backgroundColor: "#fff",
-    textAlign: "center",
+    flexDirection:"row",
+    justifyContent:"center",
   },
-
-  homeTop: {
-    display: "flex",
-    justifyContent: "center",
+  buttonStyle:{
+    backgroundColor:"#809fff",
+    borderRadius:5,
+    paddingVertical:10,
+    paddingHorizontal:20,
+    display:"flex",
+    justifyContent:"center",
     alignItems:"center",
-    paddingHorizontal : 0,
+  },
+  buttonText:{
+    fontSize:20,
+    color:"#eee",
+    // fontFamily:"JosefinSans_500Medium",
+    textTransform:"capitalize",
   },
 
-  
-  headerImage: {
-    height: "40%",
-    width : "80%",
-    aspectRatio: 19/9,
-    display: "flex",
-    alignItems:"stretch",
-    marginTop:50,
-    marginBottom:20,
-    borderRadius:20,
-  },
+});
 
-  mainHeader: {
-  fontSize: 20,
-  color:"#344055",
-  textTransform: "uppercase",
-
-  },
-  paraStyle: {
-    textAlign:"left",
-    fontSize:19, 
-    color:"#7d7d7d",
-    marginTop:30,
-    paddingBottom:50,
-    lineHeight:26,
-  },
-
-  lineStyle: {
-   marginBottom:3,  
-   borderWidth:.5,
-   borderColor:"grey",
-  },
-
-
-});    
-
-export default Home;
+export default Course ;
