e08bd516067f309596b5ca55ef0ef0af71b0055d
Nikhil Singh <87113437+Nick6398@users.noreply.github.com>
2022-06-26 15:24:38 +0530

    Create noticeapi.js

---
+++ noticeapi.js
@@ -0,0 +1,43 @@
+const Notice = [
+    {
+    id:"1",
+    title:"6th SEMESTER MINI PROJECT \n\nEVALUATION DATE ",
+        
+        
+    description:
+     "CLICK TO SEE THE DETAILS  ",
+    
+     },
+
+     {
+        id:"2",
+        title:"END SEMESTER BACK \n\nEXAMINATION DATE",
+        
+        
+        description:
+        "CLICK TO SEE THE DETAILS  ",
+       
+    },
+
+    {
+        id:"3",
+        title:"END TERM PRACTICAL EXAM \n\nFOR 4 SEM",
+        
+        description:
+        "CLICK TO SEE THE DETAILS  ",
+     
+    },
+
+    {
+        id:"4",
+        title:"4th SEMESTER EXAM \n\nSCHEDULE",
+        description:
+        "CLICK TO SEE THE DETAILS  ",
+     
+     },
+
+     
+    
+   
+];
+export default Notice;
