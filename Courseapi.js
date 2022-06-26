68268f10422a9755a2bd6cae2f73735f86e3432d
Nikhil Singh <87113437+Nick6398@users.noreply.github.com>
2022-06-26 15:23:03 +0530

    Create Courseapi.js

---
+++ Courseapi.js
@@ -0,0 +1,95 @@
+const Courses = [
+    {
+    id:"1",
+    title:"SEM 1",
+        image: require("../../assets/course/html.jpg"),
+        
+    description:
+     "",
+     course1:"                  LIST OF SUBJECTS \n\n1. Fundamental Of Computer and introduction \n  \n    to Programming",
+     course2:"2. Finite Automata and Formal Language",
+     course3:"3. Microprocessor\n\n4.Computer Organization\n\n5.java Programming LaB",
+    
+     },
+
+     {
+        id:"2",
+        title:"SEM 2",
+        image:require("../../assets/course/cn.jpg"),
+        
+        description:
+         "",
+         course1:"                  LIST OF SUBJECTS \n\n1. Programming for Problem solving",
+         course2:"2. Finite Automata and Formal Language",
+         course3:"3. Microprocessor\n\n4.Computer Organization\n\n5.java Programming Lab",
+    },
+
+    {
+        id:"3",
+        title:"SEM 3",
+        image:require("../../assets/course/cd.jpg"),
+        
+        description:
+         "",
+         course1:"                    LIST OF SUBJECTS \n\n1. Logic Design",
+         course2:"2. Data Structure with C",
+         course3:"3. OOP with C++\n\n4.Fundamentals of Cyber Security\n\n5. Fundamentals of IOT",
+    },
+
+    {
+        id:"4",
+        title:"SEM 4",
+        image:require("../../assets/course/se.jpg"),
+        description:
+         "",
+         course1:"                  LIST OF SUBJECTS \n\n1. Computer Based Numerical And Statistical\n\n    Technique",
+         course2:"2. Finite Automata and Formal Language",
+         course3:"3. Microprocessor\n\n4.Computer Organization\n\n5.java Programming Lab",
+     },
+    
+    {
+        id:"5",
+        title:"SEM 5",
+        image:require("../../assets/course/ip.jpg"),
+        description:
+            "",
+            course1:"                  LIST OF SUBJECTS \n\n\n1. System Software",
+         course2:"2. Operating System",
+         course3:"3. Data Base Management System\n\n4.Design Analysis And Algorithm\n\n5.Career Skills",
+    },
+             
+     {   
+        id:"6",
+        title:"SEM 6",
+        image:require("../../assets/course/six.jpg"),
+        description:
+        "",
+        course1:"                  LIST OF SUBJECTS \n\n1. Compiler Design",
+        course2:"2. Computer Network",
+        course3:"3. Software Engineering\n\n4.Full Stack Web Development\n\n5.Career skills",
+            },
+        
+            {   
+                id:"7",
+                title:"SEM 7",
+                image:require("../../assets/course/sev.jpg"),
+                description:
+                "",
+                course1:"                  LIST OF SUBJECTS \n\n1. Cryptography",
+                course2:"2. Advance Computer Architecture",
+                course3:"3. Artificial Intelligence\n\n4.Computer Networks - II\n\n5.Software Verification and Validation and  \n   Testing ",
+                    },
+
+                    {  
+                        id:"8",
+                        title:"SEM 8",
+                        image:require("../../assets/course/eight.jpg"),
+                        description:
+                        "",
+                        course1:"                  LIST OF SUBJECTS \n\n1. Mobile Computing",
+                        course2:"2. Disaster Management",
+                        course3:"3. Mobile Application Development\n\n4. Computer Graphics\n\n5. Soft Computing",
+                            },
+
+];
+export default Courses;
